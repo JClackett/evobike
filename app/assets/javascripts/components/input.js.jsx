f312b38269cb2029e998b880f328a9a9a4a1065c
George Borg <georgeborg00@hotmail.com>
2016-09-28 19:07:33 +0200

    labels

--- app/assets/javascripts/components/input.js.jsx
+++ app/assets/javascripts/components/input.js.jsx
@@ -15,6 +15,7 @@ var Input = React.createClass({
 	render() {
 		return (
 			<div className="input-group">
+				<h4> {this.props.name} </h4>
 		  		<input type="text" className="form-control" placeholder={this.props.value} onChange={this.handleChange} id={this.props.id} />
 			</div>
 		);
